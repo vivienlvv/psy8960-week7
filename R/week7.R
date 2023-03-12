28de874cfe1dc23efeb87f00415d60f38053a78c
Vivien Lee <vivienlvv@gmail.com>
2023-03-12 10:36:26 -0500

    Removed bold face for titles

--- R/week7.R
+++ R/week7.R
@@ -18,7 +18,7 @@ week7_tbl = read_csv("../data/week3.csv", show_col_types  = FALSE) %>%  # Using 
 
 
 
-# Visualization- Still need to follow line instruction, also need to change size of graphs (too big)
+# Visualization- Still need to follow line instruction
 (week7_tbl %>% select(starts_with("q")) %>% 
   ggpairs()) %>% 
   ggsave(filename = "../figs/fig0.png")
@@ -26,26 +26,26 @@ week7_tbl = read_csv("../data/week3.csv", show_col_types  = FALSE) %>%  # Using 
 (week7_tbl %>% ggplot(aes(x = timeStart, y = q1)) + # Figure 1 
   geom_point()  + 
   labs(x = "Date of Experiment", y = "Q1 Score") + 
-  theme(axis.title = element_text(face = "bold"), aspect.ratio = 6/10)) %>%
+  theme(aspect.ratio = 6/10)) %>%
   ggsave(filename = "../figs/fig1.png")
 
 (week7_tbl %>% ggplot(aes(x = q1, y = q2, color = gender)) + # Figure 2 
   geom_point(position = "jitter") + 
   labs(color = "Participant Gender") + 
-  theme(axis.title =  element_text(face = "bold"), legend.text = element_text(face = "bold"), legend.title = element_text(face = "bold"), aspect.ratio = 7/10)) %>% 
+  theme(aspect.ratio = 7/10)) %>% 
  ggsave(filename = "../figs/fig2.png")
 
 (week7_tbl %>% ggplot(aes(x = q1, y = q2)) + # Figure 3 
   geom_point(position = "jitter") + 
   facet_grid(cols = vars(gender), scales = "free") + 
   labs(x = "Score on Q1", y = "Score on Q2") + 
-  theme(axis.title = element_text(face = "bold"), aspect.ratio = 1)) %>% 
+  theme(aspect.ratio = 1)) %>% 
   ggsave(filename = "../figs/fig3.png")
 
 (week7_tbl %>% ggplot(aes(x = gender, y = timeSpent)) + # Figure 4 
   geom_boxplot() + 
   labs(x = "Gender", y = "Time Elapsed (mins)") + 
-  theme(axis.title = element_text(face = "bold"), aspect.ratio = 3/5)) %>% 
+  theme(aspect.ratio = 3/5)) %>% 
   ggsave(filename = "../figs/fig4.png")
 
 (week7_tbl %>% ggplot(aes(x = q5, y = q7, color = condition, fill = condition)) + # Figure 5 
@@ -54,7 +54,5 @@ week7_tbl = read_csv("../data/week3.csv", show_col_types  = FALSE) %>%  # Using 
   labs(x = "Score on Q5", y = "Score on Q7", color = "Experimental Condition", fill = "Experimental Condition") + 
   theme(legend.position ="bottom",
         legend.background = element_rect(color = "#DEDEDE", fill = "#DEDEDE"),
-        axis.title = element_text(face = "bold"), legend.title = element_text(face = "bold"), legend.text = element_text(face = "bold"), aspect.ratio = 3/7)) %>% 
+        aspect.ratio = 3/7)) %>% 
   ggsave(filename = "../figs/fig5.png")
-
-
