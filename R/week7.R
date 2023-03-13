b9484dce8da7b58bcbcee47e96b2314d7de47c24
Vivien Lee <vivienlvv@gmail.com>
2023-03-13 01:14:17 -0500

    Followed line requirements and removed unnecessary comments

--- R/week7.R
+++ R/week7.R
@@ -18,37 +18,35 @@ week7_tbl = read_csv("../data/week3.csv", show_col_types  = FALSE) %>%  # Using 
 
 
 
-# Visualization- Still need to follow line instruction
+# Visualization
 (week7_tbl %>% select(starts_with("q")) %>% 
   ggpairs()) %>% 
   ggsave(filename = "../figs/fig0.png")
-
-(week7_tbl %>% ggplot(aes(x = timeStart, y = q1)) + # Figure 1 
+(week7_tbl %>% ggplot(aes(x = timeStart, y = q1)) +  
   geom_point()  + 
   labs(x = "Date of Experiment", y = "Q1 Score") + 
   theme(aspect.ratio = 6/10)) %>%
   ggsave(filename = "../figs/fig1.png")
 
-(week7_tbl %>% ggplot(aes(x = q1, y = q2, color = gender)) + # Figure 2 
+(week7_tbl %>% ggplot(aes(x = q1, y = q2, color = gender)) +  
   geom_point(position = "jitter") + 
   labs(color = "Participant Gender") + 
   theme(aspect.ratio = 7/10)) %>% 
  ggsave(filename = "../figs/fig2.png")
-
-(week7_tbl %>% ggplot(aes(x = q1, y = q2)) + # Figure 3 
+(week7_tbl %>% ggplot(aes(x = q1, y = q2)) +  
   geom_point(position = "jitter") + 
   facet_grid(cols = vars(gender), scales = "free") + 
   labs(x = "Score on Q1", y = "Score on Q2") + 
   theme(aspect.ratio = 1)) %>% 
   ggsave(filename = "../figs/fig3.png")
 
-(week7_tbl %>% ggplot(aes(x = gender, y = timeSpent)) + # Figure 4 
+(week7_tbl %>% ggplot(aes(x = gender, y = timeSpent)) +  
   geom_boxplot() + 
   labs(x = "Gender", y = "Time Elapsed (mins)") + 
   theme(aspect.ratio = 3/5)) %>% 
   ggsave(filename = "../figs/fig4.png")
 
-(week7_tbl %>% ggplot(aes(x = q5, y = q7, color = condition, fill = condition)) + # Figure 5 
+(week7_tbl %>% ggplot(aes(x = q5, y = q7, color = condition, fill = condition)) + 
   geom_point(position = "jitter") + 
   geom_smooth(method = "lm", se = FALSE) + 
   labs(x = "Score on Q5", y = "Score on Q7", color = "Experimental Condition", fill = "Experimental Condition") + 
